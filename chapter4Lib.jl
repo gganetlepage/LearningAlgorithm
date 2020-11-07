3f68873ec0290e5b6d24de914d0783d836df93d2
guiganlep <g.ganet.lepage@gmail.com>
2020-11-07 02:38:42 +0100

    recursive max subarray done

--- chapter4Lib.jl
+++ chapter4Lib.jl
@@ -19,3 +19,43 @@ function brute_force(A::Array)
     end
     max, index_left, index_right
 end
+
+function find_max_crossing_subarray(A::Array, low::Int, mid::Int, high::Int)
+    left_sum = - typemax(Int) ÷ 2
+    sum = 0
+    max_left = -1
+    for i in reverse(low:mid)
+        sum += A[i]
+        if sum > left_sum
+            left_sum = sum
+            max_left = i
+        end
+    end
+    right_sum = - typemax(Int) ÷ 2
+    sum = 0
+    max_right = -1
+    for j in mid+1:high
+        sum += A[j]
+        if sum > right_sum
+            right_sum = sum
+            max_right = j
+        end
+    end
+    max_left, max_right, left_sum + right_sum
+end
+
+function find_max_subarray(A::Array, low::Int, high::Int)
+    high==low && return low, high, A[low]
+    mid = (low + high) ÷ 2
+    left_low, left_high, left_sum = find_max_subarray(A, low, mid)
+    right_low, right_high, right_sum = find_max_subarray(A,mid+1, high)
+    cross_low, cross_high, cross_sum = find_max_crossing_subarray(A, low, mid, high)
+    if left_sum > right_sum && left_sum > cross_sum
+        return left_low, left_high, left_sum
+    elseif right_sum > left_sum && right_sum > cross_sum
+        return right_low, right_high, right_sum
+    else
+        return cross_low, cross_high, cross_sum
+    end
+end
+
--- chapter4Script.jl
+++ chapter4Script.jl
@@ -1,6 +1,8 @@
 include("C:/Personnel/Programmation/Julia/Algorithm/chapter4Lib.jl")
-
+using Test
 # 4.1 Maximum-subarray problem
+
+# 1. when is full of nonpositive values, then find_max_subarray returns only the middle index and its associated value
 
 price1 = [100,113, 110, 85, 105,102, 86, 63, 81, 101, 94, 106, 101, 79, 94, 90, 97]
 
@@ -9,5 +11,15 @@ change1 = [ price1[i]-price1[i-1] for i in 2:length(price1)]
 price2 = [10, 11, 7,10, 6]
 change2 = [ price2[i]-price2[i-1] for i in 2:length(price2)]
 
+
+# 2. brute_force method in Θ(n²)
+brute_force(change1) # (8, 11, 43), i.e. buy before day 8 starts, sell after day 11 ends
 brute_force(change2) # (3, 3, 3), i.e. buy before day 3 starts, sell after day 3 ends
-brute_force(change1) # (43, 8, 11), i.e. buy before day 8 starts, sell after day 11 ends
+
+find_max_crossing_subarray(change1, 1, length(change1)÷2, length(change1))
+find_max_crossing_subarray(change2, 1, length(change2)÷2, length(change2))
+
+# 3. recursive algorithm
+find_max_subarray(change1, 1, length(change1)) # (8, 11, 43)
+find_max_subarray(change2, 1, length(change2)) # (3, 3, 3)
+
